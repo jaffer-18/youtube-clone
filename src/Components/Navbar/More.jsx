24e92ec7a1f73ca7f2bcc16d8ac2aaf2f7d39845
jaffer-18 <arafi7182@gmail.com>
2025-04-21 18:52:36 +0530

    more

---
+++ src/Components/Navbar/More.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './More.css';
+
+const More = () => {
+  return (
+    <div className="more-container">
+        <h1>More coming</h1>
+    </div>
+  );
+}
+
+export default More;
