71ce8a8ebfaa8c5d92c78e30946fdb742c1e6d1f
jaffer-18 <arafi7182@gmail.com>
2025-04-21 17:34:29 +0530

    hi

---
+++ src/Components/Upload/Upload.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Upload.css"
+
+const Upload = () => {
+  return (
+    <div className='main-container'>
+        <div className='upload-container'>
+            <h1>Upload</h1>
+            <div className='upload-form'>
+                <form action="">
+                    <input type="file" name="" id="" />
+                    <button type="submit"></button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Upload
