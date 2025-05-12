2a41b5c530aaaa31206174ae4a43d9b1f1892bfa
jaffer-18 <arafi7182@gmail.com>
2025-05-12 10:46:14 +0530

    removedphoto

--- src/Components/Navbar/More.jsx
+++ src/Components/Navbar/More.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import './More.css';
-import Jaffer from '../../assets/jaffer.png'; // Adjust the path as necessary
+import React from 'react'
+import './More.css'
 
 const More = () => {
   return (
-    <div className="more-container">
-        <h1>Meet the developers</h1>
-        <img src={Jaffer} className="jaffer-image" />
-        <h2>Mohammed Jaffer Sharieff</h2>
-        <p>Front end developer and data intergration</p>
+    <div className="container">
+      <h1 className="more-heading">more coming</h1>
     </div>
-  );
+  )
 }
 
-export default More;
+export default More
